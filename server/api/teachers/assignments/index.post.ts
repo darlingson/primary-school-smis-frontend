785bbecfa11174cb1b7240d1392014b248cb5132
darlingson <dmakuwila@gmail.com>
2024-06-29 10:21:12 +0200

    modified the form and endpoint to successfully assign a teacher to a subject

--- server/api/teachers/assignments/index.post.ts
+++ server/api/teachers/assignments/index.post.ts
@@ -1,39 +1,40 @@
 import TeacherSubject from "~/model/TeacherSubject"
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);  
-try {
+  const body = await readBody(event);
+  try {
     const teacherSubject = new TeacherSubject({
-      teacher: body.teacher_id,
+      teacher: body.teacher,
       subject: {
-        id: body.subject_id,
-        name: body.subject_name
+        id: body.subject.id,
+        name: body.subject.name
       },
-      class:{
-        id: body.class_id,
-        name: body.class_name
+      class: {
+        id: body.class.id,
+        name: body.class.name
       },
-      school:{
-        id: body.school_id,
-        name: body.school_name
+      school: {
+        id: body.school.id,
+        name: body.school.name
       },
       Term: body.term,
       Year: body.year,
       AcademicYear: body.academic_year
     })
-  
+
     await teacherSubject.save();
     return {
       success: true,
       message: 'Assignment created successfully'
     }
-} catch (error) {
-  let errorMessage = 'An error occurred during assignment creation';
-  if (error instanceof Error) {
-    errorMessage = error.message;
+  } catch (error) {
+    let errorMessage = 'An error occurred during assignment creation';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+      console.log(error);
+    }
+    return {
+      success: false,
+      message: errorMessage || 'An error occurred during assignment creation'
+    };
   }
-  return {
-    success: false,
-    message: errorMessage || 'An error occurred during assignment creation'
-  };
-}
 })
