9685a2792a67daf9e7d28166c8b6fd03a67813f4
darlingson <dmakuwila@gmail.com>
2024-07-01 19:53:58 +0200

    moved the endpoint that gets all students into a school/id directory so that a school can be specified and used

--- server/api/students/school/[id]/index.get.ts
+++ server/api/students/school/[id]/index.get.ts
@@ -2,9 +2,12 @@ import Student from "~/model/Student"
 
 export default defineEventHandler(async (event) => {
   //this endpoint returns all students from the school
-
+  let id = ""
+  if (event.context.params != undefined) {
+      id = event.context.params.id;
+  }
   try{
-    const students = await Student.find();
+    const students = await Student.find({ school: id });
     return {
       success:true,
       message:"Students retrieved successfully",
