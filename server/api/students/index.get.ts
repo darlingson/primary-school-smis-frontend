8de1edaf127c54f1781e9caac0af9fcffa262a57
darlingson <dmakuwila@gmail.com>
2024-07-01 19:48:13 +0200

    created endpoint for getting all students

--- server/api/students/index.get.ts
+++ server/api/students/index.get.ts
@@ -1,4 +1,24 @@
+import Student from "~/model/Student"
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
-  //this endpoint will return all students from the school
+  //this endpoint returns all students from the school
+
+  try{
+    const students = await Student.find();
+    return {
+      success:true,
+      message:"Students retrieved successfully",
+      students
+    }
+  }
+  catch(error){
+    console.log(error)
+    if(error instanceof Error){
+      return{
+        success:false,
+        message:error.message,
+        error:error
+      }
+    }
+  }
 })
