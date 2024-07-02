7b253fa5f7ab8105b1d51f97667f0493967c34cb
darlingson <dmakuwila@gmail.com>
2024-07-02 21:15:35 +0200

    created grade schema

---
+++ model/Grade.ts
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const GradeSchema = new mongoose.Schema({
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Teacher',
+    required: true
+  },
+  subject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subject',
+    required: true
+  },
+  assessmentName: {
+    type: String,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  },
+  finalGrade: {
+    type: Number,
+    required: false
+  }
+});
+
+export default mongoose.model.Grade || mongoose.model('Grade', GradeSchema)
