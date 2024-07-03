05d82487874ff269a6845213b095a53b04c43d48
darlingson <dmakuwila@gmail.com>
2024-07-03 21:26:08 +0200

    added endpoint to get grades for all students in a class

--- server/api/grades/students/[schoolID]/[classID]/index.get.ts
+++ server/api/grades/students/[schoolID]/[classID]/index.get.ts
@@ -1,3 +1,29 @@
+import Grade from "~/model/Grade"
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+  //get the grades of all students from a particular class from a particular school
+  let schoolID = ""
+  let classID = ""
+  if (event.context.params != undefined) {
+    schoolID = event.context.params.schoolID
+    classID = event.context.params.classID
+  }
+  try {
+    const grades = await Grade.find({ school: schoolID, class: classID })
+    return {
+      success: true,
+      message: 'Grades retrieved successfully',
+      grades
+    }
+  }
+  catch (error) {
+    console.log(error)
+    if (error instanceof Error) {
+      return {
+        success: false,
+        message: error.message,
+        error: error
+      }
+    }
+  }
 })
